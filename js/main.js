4bf0f78bfe62dc5e46639db07698b41e61fc904b
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-04-03 18:35:32 +0200

    zrobiono

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let tex = document.getElementById("tekst");
+document.querySelector('#dodaj').onclick = function a() {
+    tex.textContent = 'Akademia108';
+}
+
+let te = document.getElementById("tekst");
+document.querySelector('#usun').onclick = function(usun) {
+    tex.textContent = '';
+}
